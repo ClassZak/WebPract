7e2ed79054ca6f50de72d59771dc60fa40d5a553
ClassZak <pahargolub2@gmail.com>
2025-10-20 17:22:38 +0300

    Фикс выбора дат и числа человек для бронирования

--- Pract13/HotelReservation.js
+++ Pract13/HotelReservation.js
@@ -5,12 +5,12 @@ class HotelReservation{
 		if(!data)
 			throw new Error('Нет данных для создания');
 
-		this.surname		= data.surname	|| '';
+		this.surname		= data.surname		|| '';
 		this.numberPeople	= data.numberPeople || '';
-		this.rooms_count	= data.rooms_count		|| '';
+		this.rooms_count	= data.rooms_count	|| '';
 		this.inputDate		= data.inputDate	|| '';
-        this.inputDate2		= data.inputDate2	|| '';
-        this.additional		= data.additional	|| '';
-        this.age		    = data.age	|| '';
+		this.inputDate2		= data.inputDate2	|| '';
+		this.additional		= data.additional	|| '';
+		this.age			= data.age			|| '';
 	}
 }
--- Pract13/app.js
+++ Pract13/app.js
@@ -1,42 +1,43 @@
 // Требуется HotelReservation.js
 
 function handler(event){
-    alert(event.target.value);
-    const element=document.form1.inputDate2;
-    const date = this.value;
-    const timeString = date.toISOString().split('T')[0];
-    element.min = timeString;
+	const element=document.form1.inputDate2;
+	const date = event.target.value;
+	const timeString = date/*.toISOString().split('T')[0]*/;
+	element.min = timeString;
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-    setTimeout(function(){
-        document.form1.addEventListener('submit', function(event){
-            event.preventDefault();
-            console.log(event);
+	setTimeout(function(){
+		document.form1.addEventListener('submit', function(event){
+			event.preventDefault();
+			console.log(event);
 
-            const formData = new FormData(this);
-            console.log(formData);
-            
-            for(element of formData.entries())
-                console.log(element);
-            
+			const formData = new FormData(this);
+			console.log(formData);
+			
+			for(element of formData.entries())
+				console.log(element);
+			
 
-            const selectedAdditionals = formData.getAll('additional');
-            console.log(selectedAdditionals);
-            const formObject = Object.fromEntries(formData.entries());
-            formObject["additional"] = new Array(selectedAdditionals);
+			const selectedAdditionals = formData.getAll('additional');
+			console.log(selectedAdditionals);
+			const formObject = Object.fromEntries(formData.entries());
+			formObject["additional"] = new Array(selectedAdditionals);
 
-            alert(formObject);
-            HotelReservation.HotelReservations
-            .push(new HotelReservation(formObject));
-        });
+			alert(formObject);
+			HotelReservation.HotelReservations
+			.push(new HotelReservation(formObject));
+		});
 
-        {
-            const element=document.form1.inputDate;
-            const date = new Date();
-            const timeString = date.toISOString().split('T')[0];
-            element.min = timeString;
-        }
+		{
+			const element1=document.form1.inputDate;
+			const element2=document.form1.inputDate2;
+			const date = new Date();
+			const timeString = date.toISOString().split('T')[0];
+			element1.min = timeString;
+			element2.min = timeString;
+		}
 
-    }, 5);
+	}, 5);
 });
